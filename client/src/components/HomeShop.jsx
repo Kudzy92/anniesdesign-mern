fecf90cbd11f4a2c665a98f87dd936249a0c81d0
Kudzy92 <93918871+Kudzy92@users.noreply.github.com>
2023-01-16 11:02:33 +0200

    Update HomeShop.jsx

--- client/src/components/HomeShop.jsx
+++ client/src/components/HomeShop.jsx
@@ -12,6 +12,7 @@ useEffect(()=>{
   const fetchProducts= async ()=>{
     //http://localhost:3001
    const {data} = await axios.get("https://localhost:3001/api/products");
+    console.log("DATA"+data);
    setProductList(data);
   };
   fetchProducts();
